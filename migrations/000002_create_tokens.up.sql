bb4984f4eb403aec22970362a0b10e557fd42822
dzenthai <dzenthai@proton.me>
2026-06-21 00:33:44 +0500

    fix: unique user_id to unique index

--- migrations/000002_create_tokens.up.sql
+++ migrations/000002_create_tokens.up.sql
@@ -1,7 +1,10 @@
-CREATE TABLE IF NOT EXISTS tokens
-(
-    user_id    BIGINT UNIQUE               NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-    token_hash BYTEA                       NOT NULL,
-    scope      TEXT                        NOT NULL,
-    expires_at TIMESTAMP(0) WITH TIME ZONE NOT NULL
+CREATE TABLE tokens (
+                        id         BIGSERIAL PRIMARY KEY,
+                        user_id    BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+                        token_hash BYTEA NOT NULL,
+                        scope      TEXT NOT NULL,
+                        expires_at TIMESTAMP(0) WITH TIME ZONE NOT NULL
 );
+
+CREATE UNIQUE INDEX tokens_user_verification_unique
+    ON tokens (user_id) WHERE scope = 'verification';
