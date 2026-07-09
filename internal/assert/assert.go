33da7f21caffe5961c118140de6deb9238b57865
dzenthai <dzenthai@proton.me>
2026-07-09 17:24:29 +0500

    test: nil error function

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -1,10 +1,19 @@
 package assert
 
-import "testing"
+import (
+	"testing"
+)
 
 func Equal[T comparable](t *testing.T, actual, expected T) {
 	t.Helper()
 	if expected != actual {
 		t.Errorf("actual: %v, expected: %v\n", actual, expected)
+	}
+}
+
+func NilError(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err.Error())
 	}
 }
